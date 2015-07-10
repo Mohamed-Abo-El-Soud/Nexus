d135c895d114e731597446dbc55301590e940b68
MSoud <mohamed.aboelsoud@seagate.com>
2015-07-10 14:43:14 +0000

    change repository location

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,8 @@ Rails.application.routes.draw do
 
   root "static_pages#home"
   
+  # this is a test to make sure forked it right
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
